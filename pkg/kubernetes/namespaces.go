88ed9be95f67fe613e7fbad98ea0f2699a52a4d9
Mike Hepburn <eformat@gmail.com>
2025-06-10 09:19:59 +1000

    🐀 add parameter mcp descriptions 🐀

--- pkg/kubernetes/namespaces.go
+++ pkg/kubernetes/namespaces.go
@@ -14,10 +14,22 @@ func (k *Kubernetes) NamespacesList(ctx context.Context) (string, error) {
 	}, "")
 }
 
+func (k *Kubernetes) NamespaceGet(ctx context.Context, namespace string) (string, error) {
+	return k.ResourcesGet(ctx, &schema.GroupVersionKind{
+		Group: "", Version: "v1", Kind: "Namespace",
+	}, "", namespace)
+}
+
 func (k *Kubernetes) ProjectsList(ctx context.Context) (string, error) {
 	return k.ResourcesList(ctx, &schema.GroupVersionKind{
 		Group: "project.openshift.io", Version: "v1", Kind: "Project",
 	}, "")
+}
+
+func (k *Kubernetes) ProjectGet(ctx context.Context, project string) (string, error) {
+	return k.ResourcesGet(ctx, &schema.GroupVersionKind{
+		Group: "project.openshift.io", Version: "v1", Kind: "Project",
+	}, "", project)
 }
 
 func (k *Kubernetes) NamespaceCreate(ctx context.Context, namespace string, opts metav1.CreateOptions) (*corev1.Namespace, error) {
--- pkg/mcp/namespaces.go
+++ pkg/mcp/namespaces.go
@@ -14,17 +14,34 @@ func (s *Server) initNamespaces() []server.ServerTool {
 	ret := make([]server.ServerTool, 0)
 	ret = append(ret, server.ServerTool{
 		Tool: mcp.NewTool("namespaces_list",
-			mcp.WithDescription("List all the Kubernetes namespaces in the current cluster"),
+			mcp.WithDescription("List all of the Kubernetes namespaces in the current cluster"),
 		), Handler: s.namespacesList,
+	})
+	ret = append(ret, server.ServerTool{
+		Tool: mcp.NewTool("namespace_get",
+			mcp.WithDescription("Get a single Kubernetes namespace in the current cluster"),
+			mcp.WithString("namespace",
+				mcp.Required(),
+				mcp.Description("Name of the namespace to get"),
+			),
+		), Handler: s.namespaceGet,
 	})
 	ret = append(ret, server.ServerTool{
 		Tool: mcp.NewTool("namespace_create",
 			mcp.WithDescription("Create the Kubernetes namespace in the current cluster"),
+			mcp.WithString("namespace",
+				mcp.Required(),
+				mcp.Description("Name of the namespace to create"),
+			),
 		), Handler: s.namespaceCreate,
 	})
 	ret = append(ret, server.ServerTool{
 		Tool: mcp.NewTool("namespace_delete",
 			mcp.WithDescription("Delete the Kubernetes namespace in the current cluster"),
+			mcp.WithString("namespace",
+				mcp.Required(),
+				mcp.Description("Name of the namespace to delete"),
+			),
 		), Handler: s.namespaceDelete,
 	})
 	if s.k.IsOpenShift(context.Background()) {
@@ -32,6 +49,15 @@ func (s *Server) initNamespaces() []server.ServerTool {
 			Tool: mcp.NewTool("projects_list",
 				mcp.WithDescription("List all the OpenShift projects in the current cluster"),
 			), Handler: s.projectsList,
+		})
+		ret = append(ret, server.ServerTool{
+			Tool: mcp.NewTool("project_get",
+				mcp.WithDescription("Get a single OpenShift project in the current cluster"),
+				mcp.WithString("namespace",
+					mcp.Required(),
+					mcp.Description("Name of the namespace to get"),
+				),
+			), Handler: s.namespaceGet,
 		})
 	}
 	return ret
@@ -45,10 +71,34 @@ func (s *Server) namespacesList(ctx context.Context, _ mcp.CallToolRequest) (*mc
 	return NewTextResult(ret, err), nil
 }
 
+func (s *Server) namespaceGet(ctx context.Context, ctr mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	ns := ctr.Params.Arguments["namespace"]
+	if ns == nil {
+		return NewTextResult("", fmt.Errorf("failed to get namespace missing a namespace name")), nil
+	}
+	ret, err := s.k.NamespaceGet(ctx, ns.(string))
+	if err != nil {
+		err = fmt.Errorf("failed to get namespace: %v", err)
+	}
+	return NewTextResult(ret, err), nil
+}
+
 func (s *Server) projectsList(ctx context.Context, _ mcp.CallToolRequest) (*mcp.CallToolResult, error) {
 	ret, err := s.k.ProjectsList(ctx)
 	if err != nil {
 		err = fmt.Errorf("failed to list projects: %v", err)
+	}
+	return NewTextResult(ret, err), nil
+}
+
+func (s *Server) projectGet(ctx context.Context, ctr mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	ns := ctr.Params.Arguments["namespace"]
+	if ns == nil {
+		return NewTextResult("", fmt.Errorf("failed to get project missing a project name")), nil
+	}
+	ret, err := s.k.ProjectGet(ctx, ns.(string))
+	if err != nil {
+		err = fmt.Errorf("failed to get project: %v", err)
 	}
 	return NewTextResult(ret, err), nil
 }
